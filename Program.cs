49fd86a61333cec28e72aefb6dbb8e46668ced08
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-23 09:10:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cube(5));
+            cube(5);
+            Console.ReadLine();
+        }
+
+         static int cube(int number)
+        {
+            int result = number * number * number;
+            return result;
+        }
+    }
+}
